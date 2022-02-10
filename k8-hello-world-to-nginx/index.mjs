9d7bb3c73504e1adb93dbf2a6cd25311f9a30de2
Kostya <kovevare@gmail.com>
2022-02-10 11:13:56 +0200

    configs created

---
+++ k8-hello-world-to-nginx/index.mjs
@@ -0,0 +1,24 @@
+import express from 'express'
+import os from 'os'
+import fetch from 'node-fetch'
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  const helloMessage = `VERSION2: Hello from the ${os.hostname()}`;
+  console.log(helloMessage);
+  console.log('\n');
+  res.send(helloMessage);
+})
+
+app.get('/nginx', async (req, res) => {
+  const url = 'http://nginx';
+  const response = await fetch(url);
+  const body = await response.text();
+  res.send(body)
+})
+
+app.listen(PORT, () => {
+  console.log(`Web server is listening at port ${PORT}`)
+})
